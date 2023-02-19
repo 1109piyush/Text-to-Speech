564e381379692d100bd8d25e1fc1a5229c2da5bd
1109piyush <piyushsinghal11091999@gmail.com>
2023-02-19 20:28:09 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,37 @@
+# Text-to-Speech App
+
+This is a simple React application that allows you to convert text into speech using the Speech Synthesis API. You can enter text into the input box and select a voice from the dropdown menu, and then click the "Speak" button to hear the text spoken aloud.
+
+## Getting Started
+
+To get started with this app, you'll need to have [Node.js](https://nodejs.org/) installed on your computer. Once you have Node.js installed, follow these steps:
+
+1. Clone this repository to your local machine using `git clone 
+2. Navigate into the project directory using `cd text-to-speech`
+3. Install the project dependencies using `npm install`
+4. Start the development server using `npm start`
+5. Open your web browser and go to `http://localhost:3000`
+
+## Usage
+
+To use the app, simply enter text into the input box and select a voice from the dropdown menu. Then click the "Speak" button to hear the text spoken aloud. You can adjust the speed and pitch of the voice using the sliders.
+
+## Features
+
+- Convert text to speech using the Speech Synthesis API
+- Select from multiple voices
+- Adjust the speed and pitch of the voice
+
+## Technologies Used
+
+- React.js
+- CSS
+- JavaScript
+
+## Credits
+
+This app was created by piyush singhal. The text-to-speech functionality is provided by the [Speech Synthesis API](https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesis).
+
+## Code Sandbox live link
+
+ [link] (https://codesandbox.io/s/nostalgic-taussig-z8qssn?file=/src/App.css).
---
+++ src/App.js
@@ -0,0 +1,99 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function App() {
+  const [text, setText] = useState("");
+  const [voice, setVoice] = useState(null);
+  const [rate, setRate] = useState(1);
+  const [pitch, setPitch] = useState(1);
+  const [speaking, setSpeaking] = useState(false);
+  const synth = window.speechSynthesis;
+
+  useEffect(() => {
+    const onVoicesChanged = () => {
+      setVoice(synth.getVoices()[0]);
+    };
+    synth.onvoiceschanged = onVoicesChanged;
+    return () => {
+      synth.onvoiceschanged = null;
+    };
+  }, []);
+
+  const handleTextChange = (event) => {
+    setText(event.target.value);
+  };
+
+  const handleVoiceChange = (event) => {
+    setVoice(synth.getVoices()[event.target.value]);
+  };
+
+  const handleRateChange = (event) => {
+    setRate(event.target.value);
+  };
+
+  const handlePitchChange = (event) => {
+    setPitch(event.target.value);
+  };
+
+  const handleSpeak = () => {
+    const utterance = new SpeechSynthesisUtterance(text);
+    utterance.voice = voice;
+    utterance.rate = rate;
+    utterance.pitch = pitch;
+    synth.speak(utterance);
+    setSpeaking(true);
+  };
+
+  const handleStop = () => {
+    synth.cancel();
+    setSpeaking(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Piyush Text to Speech</h1>
+      <form className="form">
+        <label htmlFor="text">Enter text to speak:</label>
+        <textarea id="text" value={text} onChange={handleTextChange} />
+        <label htmlFor="voice">Select a voice:</label>
+        <select id="voice" onChange={handleVoiceChange}>
+          {synth &&
+            synth.getVoices().map((voice, index) => (
+              <option key={index} value={index}>
+                {voice.name}
+              </option>
+            ))}
+        </select>
+        <label htmlFor="rate">Rate:</label>
+        <input
+          id="rate"
+          type="range"
+          min="0.5"
+          max="2"
+          step="0.1"
+          value={rate}
+          onChange={handleRateChange}
+        />
+        <label htmlFor="pitch">Pitch:</label>
+        <input
+          id="pitch"
+          type="range"
+          min="0.5"
+          max="2"
+          step="0.1"
+          value={pitch}
+          onChange={handlePitchChange}
+        />
+        <button
+          type="button"
+          onClick={speaking ? handleStop : handleSpeak}
+          disabled={!text || !voice}
+        >
+          {speaking ? "Stop" : "Speak"}
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default App;
